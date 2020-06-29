09261edafadc378ea9454443b74c462c968e6e8e
sadisticsolution <sadisticsolutione@gmail.com>
2020-06-28 17:24:46 -0700

    Relative paths

--- packages/aa-db/build/service-worker.js
+++ packages/aa-db/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2cfe3cff319b84b9fcb4d2dd97eda29.js"
+  "./precache-manifest.9d3544956cc70dffbf242712b8d18f93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
