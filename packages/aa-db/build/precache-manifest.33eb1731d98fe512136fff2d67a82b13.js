4ac1cc67e80c0511ca90099ee5d108eedfc2784d
sadisticsolution <sadisticsolutione@gmail.com>
2020-06-29 13:28:15 -0700

    Added a caching layer for servantList query

--- packages/aa-db/build/precache-manifest.33eb1731d98fe512136fff2d67a82b13.js
+++ packages/aa-db/build/precache-manifest.33eb1731d98fe512136fff2d67a82b13.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7b247bbb10fb497a20635a84c10e3e3",
+    "revision": "16e78b9f81e2502ff9b89f7ffc131068",
     "url": "./index.html"
   },
   {
-    "revision": "fbb90088a63a49b656d7",
+    "revision": "4408a38b2fb4d0fb5f4a",
     "url": "./static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "4c7ca2109aa3552a6969",
+    "revision": "13745cc9d7494591fce4",
     "url": "./static/css/main.7db0a0b6.chunk.css"
   },
   {
-    "revision": "fbb90088a63a49b656d7",
-    "url": "./static/js/2.f1ba0e3a.chunk.js"
+    "revision": "4408a38b2fb4d0fb5f4a",
+    "url": "./static/js/2.447b1e14.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "./static/js/2.f1ba0e3a.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.447b1e14.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c7ca2109aa3552a6969",
-    "url": "./static/js/main.25b50757.chunk.js"
+    "revision": "13745cc9d7494591fce4",
+    "url": "./static/js/main.6d808f23.chunk.js"
   },
   {
     "revision": "3d03b727a1368c2d6502",
