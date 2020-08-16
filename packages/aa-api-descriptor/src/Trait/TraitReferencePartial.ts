3c9edf7899c4e14807c4fcb719475a74ef6c1f5a
sadisticsolution <sadisticsolutione@gmail.com>
2020-08-16 01:37:15 -0700

    Refactored api-connector typing

--- packages/aa-api-descriptor/src/Trait/TraitReferencePartial.ts
+++ packages/aa-api-descriptor/src/Trait/TraitReferencePartial.ts
@@ -1,8 +1,8 @@
-import Trait from "@atlasacademy/api-connector/dist/Schema/Trait";
+import {Trait} from "@atlasacademy/api-connector";
 import {ReferencePartial, ReferenceType} from "../Descriptor";
 
 export default class TraitReferencePartial extends ReferencePartial {
-    constructor(trait: Trait | number) {
+    constructor(trait: Trait.Trait | number) {
         super(ReferenceType.TRAIT, trait);
     }
 }
--- packages/aa-api-descriptor/src/Trait/describe.ts
+++ packages/aa-api-descriptor/src/Trait/describe.ts
@@ -2,7 +2,21 @@ import {Trait} from "@atlasacademy/api-connector";
 import {Descriptor, ParticlePartial, TextPartial, ValuePartial, ValueType} from "../Descriptor";
 import TraitOverrideNames from "./TraitOverrideNames";
 
-export default function (trait: Trait | number): Descriptor {
+function toTitleCase(value: string): string {
+    const matches = value.match(/[A-Z]*[a-z0-9]*/g);
+    if (!matches || !matches.length)
+        return value;
+
+    const words = matches
+        .filter(word => word.length > 0)
+        .map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        });
+
+    return words.join(' ');
+}
+
+export default function (trait: Trait.Trait | number): Descriptor {
     const id = typeof trait === 'number' ? trait : trait.id;
 
     const overrideName = TraitOverrideNames.get(id);
@@ -15,7 +29,7 @@ export default function (trait: Trait | number): Descriptor {
     const name = typeof trait === "number" ? 'unknown' : trait.name;
     if (name !== 'unknown') {
         return new Descriptor([
-            new TextPartial(name)
+            new TextPartial(toTitleCase(name))
         ]);
     }
 
