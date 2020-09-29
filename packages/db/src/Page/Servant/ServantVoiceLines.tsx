ce814bb27f636799db8b802137e701f20e406752
sadisticsolution <sadisticsolutione@gmail.com>
2020-09-29 16:37:15 -0700

    Fixed some formatting

--- packages/db/src/Page/Servant/ServantVoiceLines.tsx
+++ packages/db/src/Page/Servant/ServantVoiceLines.tsx
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import {Table, Dropdown, DropdownButton, ButtonGroup} from "react-bootstrap"
-import {Region, Servant, ProfileVoiceType} from "@atlasacademy/api-connector";
+import {ProfileVoiceType, Region, Servant} from "@atlasacademy/api-connector";
+import {toTitleCase} from "@atlasacademy/api-descriptor";
+import React, {useState} from "react";
+import {ButtonGroup, Dropdown, DropdownButton, Table} from "react-bootstrap"
 import VoiceLineAudioDescriptor from "../../Descriptor/VoiceLineAudioDescriptor";
-import { handleNewLine } from "../../Helper/OutputHelper";
-import { toTitleCase } from "@atlasacademy/api-descriptor";
+import {handleNewLine} from "../../Helper/OutputHelper";
 
 let formatSubtitle = (subtitle: string) => handleNewLine(subtitle.replace(/ *\[[^\]]*]/g, ' ').trim());
 
-export default function (props : { region: Region; servant: Servant.Servant }) {
+export default function (props: { region: Region; servant: Servant.Servant }) {
     let [playing, setPlaying] = useState('');
     let voices = props.servant?.profile?.voices;
-    let out : JSX.Element[] = [];
+    let out: JSX.Element[] = [];
 
     if (voices)
-    for (let voice of voices) {
-        let { voiceLines } = voice;
-        let lines = (
-            <Table bordered>
-                <tbody>
+        for (let voice of voices) {
+            let {voiceLines} = voice;
+            let lines = (
+                <Table bordered>
+                    <tbody>
                     {voiceLines.map((line, index) => (
                         <tr key={`line_${index}`}>
                             <td>
                                 <b>{line.overwriteName.replace("{0}", (index + 1).toString()) || line.name}</b>
-                                <br />
+                                <br/>
                                 {formatSubtitle(
-                                    (props.region === Region.JP && voice.type === ProfileVoiceType.FIRST_GET)? 
-                                    line.text.join() : line.subtitle
+                                    (props.region === Region.JP && voice.type === ProfileVoiceType.FIRST_GET) ?
+                                        line.text.join() : line.subtitle
                                 )}
                             </td>
-                            <td style={{ verticalAlign: 'middle', width: '1px' }}>
+                            <td style={{verticalAlign: 'middle', width: '1px'}}>
                                 <ButtonGroup>
                                     <VoiceLineAudioDescriptor
                                         playing={playing}
@@ -49,25 +49,25 @@ export default function (props : { region: Region; servant: Servant.Servant }) {
                             </td>
                         </tr>
                     ))}
-                </tbody>
-            </Table>
-        )
-        let row = (
-            <tr>
-                <td>{(voice.type === ProfileVoiceType.GROETH)? "Growth" : toTitleCase(voice.type)}</td>
-                <td>{lines}</td>
-            </tr>
-        )
-        out.push(row);
-    }
+                    </tbody>
+                </Table>
+            )
+            let row = (
+                <tr>
+                    <td>{(voice.type === ProfileVoiceType.GROETH) ? "Growth" : toTitleCase(voice.type)}</td>
+                    <td>{lines}</td>
+                </tr>
+            )
+            out.push(row);
+        }
 
     return (
         <Table responsive>
             <thead>
-                <tr>
-                    <td>Type</td>
-                    <td>Lines</td>
-                </tr>
+            <tr>
+                <td>Type</td>
+                <td>Lines</td>
+            </tr>
             </thead>
             <tbody>{out}</tbody>
         </Table>
