658651a5ca0819f402299b71bb3c8ad452dd104f
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2020-10-01 10:59:58 +0700

    Shrink servant main data table

--- packages/db/src/Page/Servant/ServantMainData.tsx
+++ packages/db/src/Page/Servant/ServantMainData.tsx
@@ -1,13 +1,15 @@
 import {Region, Servant} from "@atlasacademy/api-connector";
 import {toTitleCase} from "@atlasacademy/api-descriptor";
 import React from "react";
+import {Table} from "react-bootstrap";
 import ClassIcon from "../../Component/ClassIcon";
 import CommandCard from "../../Component/CommandCard";
-import DataTable from "../../Component/DataTable";
+import {Renderable} from "../../Helper/OutputHelper";
 import RawDataViewer from "../../Component/RawDataViewer";
 import CraftEssenceReferenceDescriptor from "../../Descriptor/CraftEssenceReferenceDescriptor";
 import RarityDescriptor from "../../Descriptor/RarityDescriptor";
 import {formatNumber} from "../../Helper/OutputHelper";
+import "./ServantMainData.css";
 
 interface IProps {
     region: Region;
@@ -16,7 +18,28 @@ interface IProps {
     assetId?: number;
 }
 
+type RenderableRow = {
+    title: Renderable,
+    content: Renderable
+};
+
 class ServantMainData extends React.Component<IProps> {
+    private renderDoubleRow(content: [RenderableRow, RenderableRow]): Renderable {
+        return (
+            <tr>
+                {content.map(row => <><th>{row.title}</th><td>{row.content}</td></>)}
+            </tr>
+        )
+    }
+
+    private renderSpanningRow(content: RenderableRow) : Renderable {
+        return (
+            <tr>
+                <th>{content.title}</th>
+                <td colSpan={3}>{content.content}</td>
+            </tr>
+        )
+    }
     render() {
         const servant = this.props.servant;
 
@@ -28,40 +51,65 @@ class ServantMainData extends React.Component<IProps> {
                     {servant.name}
                 </h1>
 
-                <DataTable data={{
-                    "ID": servant.id,
-                    "Collection": servant.collectionNo,
-                    "Name": servant.name,
-                    "Class": toTitleCase(servant.className),
-                    "Rarity": <RarityDescriptor rarity={servant.rarity}/>,
-                    "Cost": servant.cost,
-                    "Attribute": toTitleCase(servant.attribute),
-                    "Hp": <div>
-                        Base: {formatNumber(servant.hpBase)}
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        Max: {formatNumber(servant.hpMax)}
-                    </div>,
-                    "Atk": <div>
-                        Base: {formatNumber(servant.atkBase)}
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        Max: {formatNumber(servant.atkMax)}
-                    </div>,
-                    "Cards": <div>
-                        {servant.cards.map((card, index) => {
-                            return <CommandCard key={index}
-                                                height={60}
-                                                card={card}
-                                                servant={servant}
-                                                assetType={this.props.assetType}
-                                                assetId={this.props.assetId}/>;
+                <Table bordered className="servant-main-data-span-cell">
+                    <tbody>
+                        {this.renderDoubleRow([
+                            { title: "ID", content: servant.id }, 
+                            { title: "Collection", content: servant.collectionNo }
+                        ])}
+                        {this.renderDoubleRow([
+                            { title: "Class", content: toTitleCase(servant.className) }, 
+                            { title: "Attribute", content: toTitleCase(servant.attribute) }
+                        ])}
+                        {this.renderDoubleRow([
+                            { title: "Rarity", content: <RarityDescriptor rarity={servant.rarity}/> }, 
+                            { title: "Cost", content: servant.cost }
+                        ])}
+                        {this.renderSpanningRow({
+                            title: "HP",
+                            content: (
+                                <div>
+                                    Base: {formatNumber(servant.hpBase)}
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    Max: {formatNumber(servant.hpMax)}
+                                </div>
+                            )
                         })}
-                    </div>,
-                    "Bond CE": (
-                        servant.bondEquip
-                            ? <CraftEssenceReferenceDescriptor region={this.props.region} id={servant.bondEquip}/>
-                            : ''
-                    )
-                }}/>
+                        {this.renderSpanningRow({
+                            title: "ATK",
+                            content: (
+                                <div>
+                                    Base: {formatNumber(servant.atkBase)}
+                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                    Max: {formatNumber(servant.atkMax)}
+                                </div>
+                            )
+                        })}
+                        {this.renderSpanningRow({
+                            title: "Card",
+                            content: (
+                                <div>
+                                    {servant.cards.map((card, index) => {
+                                        return <CommandCard key={index}
+                                                            height={60}
+                                                            card={card}
+                                                            servant={servant}
+                                                            assetType={this.props.assetType}
+                                                            assetId={this.props.assetId}/>;
+                                    })}
+                                </div>
+                            )
+                        })}
+                        {this.renderSpanningRow({
+                            title: "Bond CE",
+                            content: (
+                                servant.bondEquip
+                                ? <CraftEssenceReferenceDescriptor region={this.props.region} id={servant.bondEquip}/>
+                                : ''
+                            )
+                        })}
+                    </tbody>
+                </Table>
                 <span>
                     <RawDataViewer text="Nice" data={servant}/>
                     <RawDataViewer
