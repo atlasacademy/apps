acfe6a7d8ece1259e383f8855745b48db752e31f
squaresmile <squaresmile@protonmail.com>
2020-11-18 17:35:36 -0500

    Some collaspable header changes
    - Centered the arrow vertically
    - Centered the header text vertically
    - Aligned the hr line with the start of inside content

--- packages/db/src/Component/CollapsibleContent.tsx
+++ packages/db/src/Component/CollapsibleContent.tsx
@@ -13,14 +13,15 @@ function ArrowToggle({ eventKey } : { eventKey: string }) {
 function renderCollapsibleContent({ title, content, subheader } : { title: Renderable, content: Renderable, subheader: boolean }) {
     return (
         <Accordion defaultActiveKey={`${title}`}>
-            <Card border="light" className="collapsible-card">  
+            <Card border="light" className="collapsible-card">
                 <hr className="collapsible-header-separator" />
                 <Accordion.Toggle className="collapsible-header" as="div" eventKey={`${title}`}>
-                    {subheader ? <h5 style={{ display: "inline" }}>{title}</h5> : <h3 style={{ display: 'inline' }}>{title}</h3>}
-                    <span style={{ flexGrow: 1, float: "right" }}><ArrowToggle eventKey={`${title}`}/></span>
+                    {subheader ? <h5 style={{ marginBottom: 0 }}>{title}</h5> : <h3 style={{ marginBottom: 0 }}>{title}</h3>}
+                    <span style={{ textAlign: 'right', alignSelf: 'center', marginRight: '1em' }}><ArrowToggle eventKey={`${title}`}/></span>
                 </Accordion.Toggle>
+                {/* 2 px to align the hr line with the start of content */}
                 <Accordion.Collapse eventKey={`${title}`}>
-                    <div>{content}</div>
+                    <div style={{ marginTop: '2px', marginBottom: '1em' }}>{content}</div>
                 </Accordion.Collapse>
             </Card>
         </Accordion>
