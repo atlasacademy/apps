e711ec00ea9a086e8594dcceb189629e2bfe34e1
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2020-11-18 23:38:13 +0700

    List ascension conditions of voice lines as a range if possible

--- packages/db/src/Descriptor/VoicePrefixDescriptor.tsx
+++ packages/db/src/Descriptor/VoicePrefixDescriptor.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import type {Servant} from "@atlasacademy/api-connector";
 export default (props : { currentVoicePrefix: number, ascensionAdd: Servant.Servant['ascensionAdd'] }) => {
     let { ascensionAdd: { voicePrefix }, currentVoicePrefix } = props;
-    let ascensionConds = Object.entries(voicePrefix.ascension || {})
+    let ascConds = Object.entries(voicePrefix.ascension || {})
         .filter(([_, prefix]) => prefix === currentVoicePrefix)
         .map(asc => +asc[0])
     let costumeConds = Object.entries(voicePrefix.costume || {})
         .filter(([_, prefix]) => prefix === currentVoicePrefix)
         .map(costumeId => +costumeId[0]);
+    // write a range instead of listing all levels
+    // if contiguous range
+    let isValidRange = (Math.max(...ascConds) - Math.min(...ascConds) + 1) === ascConds.length;
+    let ascString = isValidRange ? `${ascConds.sort()[0]} – ${ascConds.sort().pop()}` : ascConds.join(' / ')
     return (
         <>
-            {ascensionConds.length ? <>Ascension {ascensionConds.join(' / ')}&nbsp;</> : ''}
-            {costumeConds.length ? <>{ascensionConds.length ? 'or c' : 'C'}ostume ID {costumeConds.join(' / ')}</> : ''}
+            {ascConds.length ? <>Ascension {ascString}&nbsp;</> : ''}
+            {costumeConds.length ? <>{ascConds.length ? 'or c' : 'C'}ostume ID {costumeConds.join(' / ')}</> : ''}
         </>
     )
 }
