5d8fa6d51a037f1d4da49e428aa623c9894f0d41
squaresmile <squaresmile@protonmail.com>
2021-02-01 19:26:00 -0500

    Added item icon for item drop up function

--- packages/db/src/Descriptor/ItemDescriptor.tsx
+++ packages/db/src/Descriptor/ItemDescriptor.tsx
@@ -2,6 +2,17 @@ import { Region, Item } from "@atlasacademy/api-connector";
 import { useState, useEffect } from "react";
 import Api from "../Api";
 import TraitDescription from "./TraitDescription";
+import ItemIcon from "../Component/ItemIcon";
+import { mergeElements } from "../Helper/OutputHelper";
+
+function getItemWithIcon(region: Region, item: Item.Item) {
+  return (
+    <>
+      <ItemIcon region={region} item={item} />
+      {item.name}
+    </>
+  );
+}
 
 export default function ItemDescriptor(props: {
   region: Region;
@@ -12,7 +23,14 @@ export default function ItemDescriptor(props: {
     Api.searchItem(undefined, [props.individuality]).then((s) => setItems(s));
   }, [props.region, props.individuality]);
   if (items && items.length > 0) {
-    return <>{items.map((item) => item.name).join(" and ")}</>;
+    return (
+      <>
+        {mergeElements(
+          items.map((item) => getItemWithIcon(props.region, item)),
+          " and "
+        )}
+      </>
+    );
   } else {
     return (
       <TraitDescription region={props.region} trait={props.individuality} />
