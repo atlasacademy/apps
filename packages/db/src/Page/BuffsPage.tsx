b179ffb39887998ec8fe00788957ae68e188ee42
squaresmile <squaresmile@protonmail.com>
2021-02-18 20:04:19 -0500

    Format the files with prettier

--- packages/db/src/Page/BuffsPage.tsx
+++ packages/db/src/Page/BuffsPage.tsx
@@ -1,10 +1,10 @@
-import {Buff, Region} from "@atlasacademy/api-connector";
-import {BuffDescriptor} from "@atlasacademy/api-descriptor";
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {AxiosError} from "axios";
+import { Buff, Region } from "@atlasacademy/api-connector";
+import { BuffDescriptor } from "@atlasacademy/api-descriptor";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AxiosError } from "axios";
 import React from "react";
-import {Button, Form, Table} from "react-bootstrap";
+import { Button, Form, Table } from "react-bootstrap";
 import Api from "../Api";
 import ErrorStatus from "../Component/ErrorStatus";
 import Loading from "../Component/Loading";
@@ -14,9 +14,7 @@ import Manager from "../Setting/Manager";
 
 let stateCache = new Map<Region, IState>([]);
 
-interface ChangeEvent extends React.ChangeEvent<HTMLInputElement> {
-
-}
+interface ChangeEvent extends React.ChangeEvent<HTMLInputElement> {}
 
 interface IProps {
     region: Region;
@@ -32,7 +30,7 @@ interface IState {
 }
 
 const buffDescriptions = new Map<Buff.BuffType, string>(
-    Object.values(Buff.BuffType).map(type => {
+    Object.values(Buff.BuffType).map((type) => {
         return [type, BuffDescriptor.describeType(type)];
     })
 );
@@ -44,7 +42,7 @@ class BuffsPage extends React.Component<IProps, IState> {
         let state = stateCache.get(props.region) ?? {
             searching: false,
             searched: false,
-            buffs: []
+            buffs: [],
         };
 
         if (state?.error) state.error = undefined;
@@ -54,106 +52,122 @@ class BuffsPage extends React.Component<IProps, IState> {
     }
 
     componentDidUpdate() {
-        stateCache.set(this.props.region, {...this.state});
+        stateCache.set(this.props.region, { ...this.state });
     }
 
     componentDidMount() {
-        document.title = `[${this.props.region}] Buffs - Atlas Academy DB`
+        document.title = `[${this.props.region}] Buffs - Atlas Academy DB`;
     }
 
     private async search() {
         // no filter set
         if (!this.state.name && !this.state.type) {
-            this.setState({buffs: []});
-            alert('Please refine the results before searching');
+            this.setState({ buffs: [] });
+            alert("Please refine the results before searching");
             return;
         }
 
         try {
-            await this.setState({searching: true, buffs: []});
+            await this.setState({ searching: true, buffs: [] });
 
             const buffs = await Api.searchBuffs(
                 this.state.name,
                 this.state.type
             );
 
-            this.setState({buffs, searched: true});
+            this.setState({ buffs, searched: true });
         } catch (e) {
             this.setState({
-                error: e
+                error: e,
             });
         } finally {
-            this.setState({ searching: false })
+            this.setState({ searching: false });
         }
     }
 
     render() {
-        if (this.state.error)
-            return <ErrorStatus error={this.state.error}/>;
+        if (this.state.error) return <ErrorStatus error={this.state.error} />;
 
         let table = (
             <Table responsive>
                 <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Buff</th>
-                    <th>Usage Count</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>Buff</th>
+                        <th>Usage Count</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {this.state.buffs.map((buff, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{buff.id}</td>
-                            <td>
-                                <BuffDescription region={this.props.region} buff={buff}/>
-                            </td>
-                            <td>
-                                {(buff.reverse?.basic?.function ?? []).length}
-                            </td>
-                        </tr>
-                    )
-                })}
+                    {this.state.buffs.map((buff, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{buff.id}</td>
+                                <td>
+                                    <BuffDescription
+                                        region={this.props.region}
+                                        buff={buff}
+                                    />
+                                </td>
+                                <td>
+                                    {
+                                        (buff.reverse?.basic?.function ?? [])
+                                            .length
+                                    }
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </Table>
         );
 
         return (
             <div>
-                {this.state.searching ? <Loading/> : null}
+                {this.state.searching ? <Loading /> : null}
 
                 <h1>Buffs Search</h1>
 
-                <form onSubmit={(ev: React.FormEvent) => {
-                    ev.preventDefault();
-                    this.search();
-                }}>
+                <form
+                    onSubmit={(ev: React.FormEvent) => {
+                        ev.preventDefault();
+                        this.search();
+                    }}
+                >
                     <Form.Group>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control value={this.state.name ?? ''}
-                                      onChange={(ev: ChangeEvent) => {
-                                          this.setState({name: ev.target.value});
-                                      }}/>
+                        <Form.Control
+                            value={this.state.name ?? ""}
+                            onChange={(ev: ChangeEvent) => {
+                                this.setState({ name: ev.target.value });
+                            }}
+                        />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Type</Form.Label>
-                        <SearchableSelect<Buff.BuffType> id='select-BuffType'
-                                                         options={Object.values(Buff.BuffType)}
-                                                         labels={buffDescriptions}
-                                                         selected={this.state.type ? this.state.type[0] : undefined}
-                                                         onChange={(value?: Buff.BuffType) => {
-                                                             this.setState({type: value ? [value] : []});
-                                                         }}/>
+                        <SearchableSelect<Buff.BuffType>
+                            id="select-BuffType"
+                            options={Object.values(Buff.BuffType)}
+                            labels={buffDescriptions}
+                            selected={
+                                this.state.type ? this.state.type[0] : undefined
+                            }
+                            onChange={(value?: Buff.BuffType) => {
+                                this.setState({ type: value ? [value] : [] });
+                            }}
+                        />
                     </Form.Group>
-                    <Button variant={'primary'} onClick={() => this.search()}>
-                        Search
-                        {' '}
-                        <FontAwesomeIcon icon={faSearch}/>
+                    <Button variant={"primary"} onClick={() => this.search()}>
+                        Search <FontAwesomeIcon icon={faSearch} />
                     </Button>
                 </form>
 
-                <hr/>
-                {this.state.searched && <h5>Found <b>{this.state.buffs.length}</b> result{this.state.buffs.length > 1 ? 's' : ''}.</h5>}
+                <hr />
+                {this.state.searched && (
+                    <h5>
+                        Found <b>{this.state.buffs.length}</b> result
+                        {this.state.buffs.length > 1 ? "s" : ""}.
+                    </h5>
+                )}
                 {this.state.buffs.length ? table : null}
             </div>
         );
--- packages/db/src/Page/FuncsPage.tsx
+++ packages/db/src/Page/FuncsPage.tsx
@@ -1,23 +1,21 @@
-import {Func, Region} from "@atlasacademy/api-connector";
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {AxiosError} from "axios";
+import { Func, Region } from "@atlasacademy/api-connector";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AxiosError } from "axios";
 import React from "react";
-import {Button, Form, Table} from "react-bootstrap";
+import { Button, Form, Table } from "react-bootstrap";
 import Api from "../Api";
 import ErrorStatus from "../Component/ErrorStatus";
 import Loading from "../Component/Loading";
 import SearchableSelect from "../Component/SearchableSelect";
-import {funcDescriptions} from "../Descriptor/Func/handleActionSection";
-import {targetDescriptions} from "../Descriptor/Func/handleTargetSection";
+import { funcDescriptions } from "../Descriptor/Func/handleActionSection";
+import { targetDescriptions } from "../Descriptor/Func/handleTargetSection";
 import FuncDescriptor from "../Descriptor/FuncDescriptor";
 import Manager from "../Setting/Manager";
 
 let stateCache = new Map<Region, IState>([]);
 
-interface ChangeEvent extends React.ChangeEvent<HTMLInputElement> {
-
-}
+interface ChangeEvent extends React.ChangeEvent<HTMLInputElement> {}
 
 interface IProps {
     region: Region;
@@ -41,33 +39,37 @@ class FuncsPage extends React.Component<IProps, IState> {
         let state = stateCache.get(props.region) ?? {
             searching: false,
             searched: false,
-            funcs: []
+            funcs: [],
         };
         if (state?.error) state.error = undefined;
         this.state = state;
-
 
         Manager.setRegion(this.props.region);
     }
 
     componentDidMount() {
-        document.title = `[${this.props.region}] Functions - Atlas Academy DB`
+        document.title = `[${this.props.region}] Functions - Atlas Academy DB`;
     }
 
     componentDidUpdate() {
-        stateCache.set(this.props.region, {...this.state});
+        stateCache.set(this.props.region, { ...this.state });
     }
 
     private async search() {
         // no filter set
-        if (!this.state.popupText && !this.state.type && !this.state.targetType && !this.state.targetTeam) {
-            this.setState({funcs: []});
-            alert('Please refine the results before searching');
+        if (
+            !this.state.popupText &&
+            !this.state.type &&
+            !this.state.targetType &&
+            !this.state.targetTeam
+        ) {
+            this.setState({ funcs: [] });
+            alert("Please refine the results before searching");
             return;
         }
 
         try {
-            await this.setState({searching: true, funcs: []});
+            await this.setState({ searching: true, funcs: [] });
 
             const funcs = await Api.searchFuncs(
                 this.state.popupText,
@@ -76,113 +78,149 @@ class FuncsPage extends React.Component<IProps, IState> {
                 this.state.targetTeam
             );
 
-            this.setState({funcs, searched: true});
+            this.setState({ funcs, searched: true });
         } catch (e) {
             this.setState({
-                error: e
+                error: e,
             });
         } finally {
             this.setState({
-                searching: false
-            })
+                searching: false,
+            });
         }
     }
 
     render() {
-        if (this.state.error)
-            return <ErrorStatus error={this.state.error}/>;
+        if (this.state.error) return <ErrorStatus error={this.state.error} />;
 
         let table = (
             <Table responsive>
                 <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Function</th>
-                    <th>Usage Count</th>
-                </tr>
+                    <tr>
+                        <th>#</th>
+                        <th>Function</th>
+                        <th>Usage Count</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {this.state.funcs.map((func, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{func.funcId}</td>
-                            <td>
-                                <FuncDescriptor region={this.props.region} func={func}/>
-                            </td>
-                            <td>
-                                {
-                                    (func.reverse?.basic?.NP ?? []).length
-                                    + (func.reverse?.basic?.skill ?? []).length
-                                }
-                            </td>
-                        </tr>
-                    )
-                })}
+                    {this.state.funcs.map((func, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{func.funcId}</td>
+                                <td>
+                                    <FuncDescriptor
+                                        region={this.props.region}
+                                        func={func}
+                                    />
+                                </td>
+                                <td>
+                                    {(func.reverse?.basic?.NP ?? []).length +
+                                        (func.reverse?.basic?.skill ?? [])
+                                            .length}
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </Table>
         );
 
         return (
             <div>
-                {this.state.searching ? <Loading/> : null}
+                {this.state.searching ? <Loading /> : null}
 
                 <h1>Functions Search</h1>
 
-                <form onSubmit={(ev: React.FormEvent) => {
-                    ev.preventDefault();
-                    this.search();
-                }}>
+                <form
+                    onSubmit={(ev: React.FormEvent) => {
+                        ev.preventDefault();
+                        this.search();
+                    }}
+                >
                     <Form.Group>
                         <Form.Label>Text</Form.Label>
-                        <Form.Control value={this.state.popupText ?? ''}
-                                      onChange={(ev: ChangeEvent) => {
-                                          this.setState({popupText: ev.target.value});
-                                      }}/>
+                        <Form.Control
+                            value={this.state.popupText ?? ""}
+                            onChange={(ev: ChangeEvent) => {
+                                this.setState({ popupText: ev.target.value });
+                            }}
+                        />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Type</Form.Label>
-                        <SearchableSelect<Func.FuncType> id='select-FuncType'
-                                                         options={Object.values(Func.FuncType)}
-                                                         labels={funcDescriptions}
-                                                         selected={this.state.type ? this.state.type[0] : undefined}
-                                                         onChange={(value?: Func.FuncType) => {
-                                                             this.setState({type: value ? [value] : []});
-                                                         }}/>
+                        <SearchableSelect<Func.FuncType>
+                            id="select-FuncType"
+                            options={Object.values(Func.FuncType)}
+                            labels={funcDescriptions}
+                            selected={
+                                this.state.type ? this.state.type[0] : undefined
+                            }
+                            onChange={(value?: Func.FuncType) => {
+                                this.setState({ type: value ? [value] : [] });
+                            }}
+                        />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Target</Form.Label>
-                        <SearchableSelect<Func.FuncTargetType> id='select-FuncTargetType'
-                                                               options={Object.values(Func.FuncTargetType)}
-                                                               labels={targetDescriptions}
-                                                               selected={this.state.targetType ? this.state.targetType[0] : undefined}
-                                                               onChange={(value?: Func.FuncTargetType) => {
-                                                                   this.setState({targetType: value ? [value] : []});
-                                                               }}/>
+                        <SearchableSelect<Func.FuncTargetType>
+                            id="select-FuncTargetType"
+                            options={Object.values(Func.FuncTargetType)}
+                            labels={targetDescriptions}
+                            selected={
+                                this.state.targetType
+                                    ? this.state.targetType[0]
+                                    : undefined
+                            }
+                            onChange={(value?: Func.FuncTargetType) => {
+                                this.setState({
+                                    targetType: value ? [value] : [],
+                                });
+                            }}
+                        />
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Affects Players/Enemies</Form.Label>
-                        <SearchableSelect<Func.FuncTargetTeam> id='select-FuncTargetTeam'
-                                                               options={Object.values(Func.FuncTargetTeam)}
-                                                               labels={new Map<Func.FuncTargetTeam, string>([
-                                                                   [Func.FuncTargetTeam.PLAYER_AND_ENEMY, 'Players and Enemies'],
-                                                                   [Func.FuncTargetTeam.PLAYER, 'Players only'],
-                                                                   [Func.FuncTargetTeam.ENEMY, 'Enemies only'],
-                                                               ])}
-                                                               selected={this.state.targetTeam ? this.state.targetTeam[0] : undefined}
-                                                               onChange={(value?: Func.FuncTargetTeam) => {
-                                                                   this.setState({targetTeam: value ? [value] : []});
-                                                               }}/>
+                        <SearchableSelect<Func.FuncTargetTeam>
+                            id="select-FuncTargetTeam"
+                            options={Object.values(Func.FuncTargetTeam)}
+                            labels={
+                                new Map<Func.FuncTargetTeam, string>([
+                                    [
+                                        Func.FuncTargetTeam.PLAYER_AND_ENEMY,
+                                        "Players and Enemies",
+                                    ],
+                                    [
+                                        Func.FuncTargetTeam.PLAYER,
+                                        "Players only",
+                                    ],
+                                    [Func.FuncTargetTeam.ENEMY, "Enemies only"],
+                                ])
+                            }
+                            selected={
+                                this.state.targetTeam
+                                    ? this.state.targetTeam[0]
+                                    : undefined
+                            }
+                            onChange={(value?: Func.FuncTargetTeam) => {
+                                this.setState({
+                                    targetTeam: value ? [value] : [],
+                                });
+                            }}
+                        />
                     </Form.Group>
-                    <Button variant={'primary'} onClick={() => this.search()}>
-                        Search
-                        {' '}
-                        <FontAwesomeIcon icon={faSearch}/>
+                    <Button variant={"primary"} onClick={() => this.search()}>
+                        Search <FontAwesomeIcon icon={faSearch} />
                     </Button>
                 </form>
 
-                <hr/>
+                <hr />
 
-                {this.state.searched && <h5>Found <b>{this.state.funcs.length}</b> result{this.state.funcs.length > 1 ? 's' : ''}.</h5>}
+                {this.state.searched && (
+                    <h5>
+                        Found <b>{this.state.funcs.length}</b> result
+                        {this.state.funcs.length > 1 ? "s" : ""}.
+                    </h5>
+                )}
                 {this.state.funcs.length ? table : null}
             </div>
         );
