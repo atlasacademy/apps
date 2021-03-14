b55a029ccb1a0370a31494997761cf44d20222e9
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-03-14 13:27:30 +0700

    Only allow an audio source to play at a single time

---
+++ packages/db/src/Descriptor/VoiceLine.tsx
@@ -0,0 +1,77 @@
+class AudioElement {
+    private element: HTMLAudioElement = new Audio();
+    private url: string;
+    private loaded: boolean = false;
+
+    constructor(url: string) {
+        this.url = url;
+    };
+
+    load() {
+        return new Promise(res => {
+            if (this.loaded) {
+                res(undefined);
+
+                return;
+            }
+
+            const element = new Audio(this.url);
+            element.volume = 0.5;
+
+            element.addEventListener('loadeddata', () => {
+                element.pause();
+                element.currentTime = 0;
+                this.loaded = true;
+
+                res(undefined);
+            });
+
+            element.play();
+            this.element = element;
+        });
+    }
+
+    play() {
+        return new Promise(res => {
+            this.element.onpause = this.element.onerror = this.element.onended = () => {
+                this.stop();
+                res(undefined);
+            };
+            this.element.play();
+        });
+    }
+
+    stop() {
+        this.element.pause();
+        this.element.currentTime = 0;
+    };
+}
+
+export class VoiceLine {
+    private voiceLines: { audio: AudioElement, delay: number }[]
+    current?: AudioElement;
+    stopping?: boolean;
+
+    constructor(assets : [string, number][]) {
+        this.voiceLines = assets.map(_ => ({ audio: new AudioElement(_[0]), delay: _[1] }));
+    }
+
+    async play() {
+        for (let { audio } of this.voiceLines) await audio.load();
+        for (let line of this.voiceLines) {
+            if (this.stopping) break;
+            await new Promise(resolve => setTimeout(resolve, line.delay * 1000));
+            this.current = line.audio;
+            await line.audio.play();
+        }
+
+        this.current = undefined;
+        this.stopping = false;
+    }
+
+    async stop() {
+        if (!this.current) return this.stopping = false;
+        this.stopping = true;
+        this.current?.stop();
+    }
+}
--- packages/db/src/Descriptor/VoiceLinePlayer.tsx
+++ packages/db/src/Descriptor/VoiceLinePlayer.tsx
@@ -2,55 +2,9 @@ import {faPlay, faStop} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
 import {Button} from "react-bootstrap";
+import {VoiceLine} from "./VoiceLine";
 
-class AudioElement {
-    private element: HTMLAudioElement = new Audio();
-    private url: string;
-    private loaded: boolean = false;
-
-    constructor(url: string) {
-        this.url = url;
-    };
-
-    load() {
-        return new Promise(res => {
-            if (this.loaded) {
-                res(undefined);
-
-                return;
-            }
-
-            const element = new Audio(this.url);
-            element.volume = 0.5;
-
-            element.addEventListener('loadeddata', () => {
-                element.pause();
-                element.currentTime = 0;
-                this.loaded = true;
-
-                res(undefined);
-            });
-
-            element.play();
-            this.element = element;
-        });
-    }
-
-    play() {
-        return new Promise(res => {
-            this.element.onpause = this.element.onerror = this.element.onended = () => {
-                this.stop();
-                res(undefined);
-            };
-            this.element.play();
-        });
-    }
-
-    stop() {
-        this.element.pause();
-        this.element.currentTime = 0;
-    };
-}
+const VoiceLineStorage = new Map<string, VoiceLine>();
 
 interface IProps {
     audioAssetUrls: string[];
@@ -59,101 +13,64 @@ interface IProps {
 
 interface IState {
     playing: boolean;
-    control: AudioElement | undefined;
-    started: number | undefined;
 }
 
 class VoiceLinePlayer extends React.Component<IProps, IState> {
-    private readonly audioControllers: AudioElement[];
-
     constructor(props: IProps) {
         super(props);
-
-        this.audioControllers = props.audioAssetUrls.map(url => new AudioElement(url));
-
-        this.state = {
-            playing: false,
-            control: undefined,
-            started: undefined
-        };
+        this.state = { playing: false };
+        VoiceLineStorage.set(
+            props.audioAssetUrls[0],
+            new VoiceLine(props.audioAssetUrls.map((url, index) => [url, props.delay[index]]))
+        );
     }
 
     componentDidUpdate(prevProps : IProps) {
         // compare urls. stop if they change.
-        if (this.props.audioAssetUrls.length != prevProps.audioAssetUrls.length) return void this.stop();
+        let reset = () => {
+            VoiceLineStorage.get(this.getVoiceLineKey(prevProps.audioAssetUrls))?.stop();
+            VoiceLineStorage.delete(this.getVoiceLineKey(prevProps.audioAssetUrl));
+            VoiceLineStorage.set(
+                this.getVoiceLineKey(this.props.audioAssetUrls),
+                new VoiceLine(this.props.audioAssetUrls.map((url, index) => [url, this.props.delay[index]]))
+            );
+        }
+
+        if (this.props.audioAssetUrls.length != prevProps.audioAssetUrls.length) return void reset();
         for (let index in this.props.audioAssetUrls)
-            if (this.props.audioAssetUrls[index] != prevProps.audioAssetUrls[index]) return void this.stop();
+            if (this.props.audioAssetUrls[index] != prevProps.audioAssetUrls[index]) return void reset();
     }
 
     componentWillUnmount() {
         this.stop();
+        VoiceLineStorage.delete(this.getVoiceLineKey(this.props.audioAssetUrls));
     }
 
-    private isPlaying(started: number): boolean {
-        return this.state.playing && started === this.state.started;
-    }
-
-    private async onClick() {
+    private onClick = async () => {
+        const control = VoiceLineStorage.get(this.getVoiceLineKey(this.props.audioAssetUrls));
         if (this.state.playing)
-            return this.stop();
+            return control?.stop().then(() => this.setState({ playing: false }))
 
-        const started = Date.now();
-
-        await this.setState({
-            playing: true,
-            started
-        });
-
-        await Promise.all(this.audioControllers.map(async (control) => {
-            await control.load();
-
-            return;
-        }));
-
-        this.playLine(0, started);
+        this.setState({ playing: true });
+        for (let [key, player] of VoiceLineStorage) if (key != this.getVoiceLineKey(this.props.audioAssetUrls)) player.stop();
+        control?.play().then(() => this.setState({ playing: false }));
     }
-
-    private async playLine(index: number, started: number) {
-        const control = this.audioControllers[index] ?? undefined,
-            delay = this.props.delay[index] ?? undefined;
-
-        if (!this.isPlaying(started) || !control || delay === undefined) {
-            return this.stop();
-        }
-
-        await new Promise(resolve => {
-            setTimeout(resolve, delay * 1000);
-        });
-
-        if (!this.isPlaying(started))
-            return this.stop();
-
-        this.setState({control});
-        await control.play();
-
-        if (this.isPlaying(started))
-            this.playLine(index + 1, started);
-    };
 
     private stop() {
-        const control = this.state.control;
+        VoiceLineStorage.get(this.getVoiceLineKey(this.props.audioAssetUrls))?.stop();
+        this.setState({ playing: false });
+    }
 
-        this.setState({
-            playing: false,
-            control: undefined,
-            started: undefined
-        });
-
-        control?.stop();
+    private getVoiceLineKey(urls : string[]) {
+        // derive an unique key across voice lines
+        return urls[0];
     }
 
     render() {
         return (
             <Button
                 variant={this.state.playing ? 'warning' : 'success'}
-                onClick={() => {
-                    this.onClick();
-                }}
+                onClick={this.onClick}
                 style={{whiteSpace: "nowrap"}}>
                 <FontAwesomeIcon icon={this.state.playing ? faStop : faPlay}/>
             </Button>
