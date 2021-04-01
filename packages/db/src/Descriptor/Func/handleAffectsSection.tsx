872f1d2cbdd7c4edb1745a72f15a8e5f3ee3692b
squaresmile <squaresmile@protonmail.com>
2021-03-31 20:00:57 -0400

    Better descriptions for functarget self + available functvals

--- packages/db/src/Descriptor/Func/handleAffectsSection.tsx
+++ packages/db/src/Descriptor/Func/handleAffectsSection.tsx
@@ -118,7 +118,11 @@ export default function handleAffectsSection(region: Region, sections: FuncDescr
     }
 
     if (func.functvals.length) {
-        parts.push('for targets');
+        if (func.funcTargetType === Func.FuncTargetType.SELF) {
+            parts.push('if self has');
+        } else {
+            parts.push('for targets with');
+        }
 
         if (func.functvals.length > 1) {
             parts.push('all');
