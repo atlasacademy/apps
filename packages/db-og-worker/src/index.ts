1225db5bbdd75150c767ca8d733c14fa4077a548
squaresmile <squaresmile@protonmail.com>
2021-04-28 21:30:52 -0400

    Added ts-loader and prettier to db worker

--- packages/db-og-worker/src/index.ts
+++ packages/db-og-worker/src/index.ts
@@ -1,35 +1,51 @@
-addEventListener('fetch', (event) => {
-    const response = handleRequest(event.request).catch(() => new Response('error', { status: 500 }))
-    event.respondWith(response)
-})
+addEventListener("fetch", (event) => {
+    const response = handleRequest(event.request).catch(
+        () => new Response("error", { status: 500 })
+    );
+    event.respondWith(response);
+});
 
 class Handler {
     content = "";
-    constructor(content : string = "") { this.content = content || this.content; }
-    element(elem : Element) {
-        elem.setAttribute('content', this.content || elem.getAttribute('content')!);
+    constructor(content: string = "") {
+        this.content = content || this.content;
+    }
+    element(elem: Element) {
+        elem.setAttribute(
+            "content",
+            this.content || elem.getAttribute("content")!
+        );
     }
 }
 
-function overwrite(_ : Response, overwrite_ : string) {
-    return new HTMLRewriter().on('[property="og:title"]', new Handler(overwrite_)).transform(_);
+function overwrite(_: Response, overwrite_: string) {
+    return new HTMLRewriter()
+        .on('[property="og:title"]', new Handler(overwrite_))
+        .transform(_);
 }
 
-async function handleRequest(request : Request) {
+async function handleRequest(request: Request) {
     let url = new URL(request.url);
     let { pathname } = url;
 
-    if (pathname.startsWith('/db/')) pathname = pathname.replace('/db/', '');
+    if (pathname.startsWith("/db/")) pathname = pathname.replace("/db/", "");
 
-    let [region, subpage, target, ...paths] = pathname.split('/').filter(Boolean);
+    let [region, subpage, target, ...paths] = pathname
+        .split("/")
+        .filter(Boolean);
 
-    url.hostname = 'apps.atlasacademy.io';
+    url.hostname = "apps.atlasacademy.io";
     let page = await fetch(url.href, request);
 
     switch (subpage) {
         case `servant`: {
-            let { name } = await fetch(`https://api.atlasacademy.io/basic/${region}/servant/${target}?lang=en`).then(res => res.json());
-            return overwrite(page, `[${region}] Servant - ${name} - Atlas Academy DB`);
+            let { name } = await fetch(
+                `https://api.atlasacademy.io/basic/${region}/servant/${target}?lang=en`
+            ).then((res) => res.json());
+            return overwrite(
+                page,
+                `[${region}] Servant - ${name} - Atlas Academy DB`
+            );
         }
         default:
             return new Response(page.body, page);
