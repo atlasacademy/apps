264802dd6cc3393a90deaadcccad1b1efbcbd59d
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-04-29 07:31:21 +0700

    Overwrite OG metadata for servant routes

--- packages/db-og-worker/src/index.ts
+++ packages/db-og-worker/src/index.ts
@@ -7,18 +7,31 @@ class Handler {
     content = "";
     constructor(content : string = "") { this.content = content || this.content; }
     element(elem : Element) {
-        elem.setAttribute('content', elem.getAttribute('content')! + '\n' + this.content);
+        elem.setAttribute('content', this.content || elem.getAttribute('content')!);
     }
 }
 
+function overwrite(_ : Response, overwrite_ : string) {
+    return new HTMLRewriter().on('[property="og:title"]', new Handler(overwrite_)).transform(_);
+}
+
 async function handleRequest(request : Request) {
-    var url = new URL(request.url);
-    if (url.pathname === '/') {
-        let _ = await fetch('https://apps.atlasacademy.io/db/');
-        return new HTMLRewriter().on('[property="og:description"]', new Handler(request.url)).transform(_);
-    }
-    else {
-        url.hostname = 'apps.atlasacademy.io';
-        return fetch(url.href, request);
+    let url = new URL(request.url);
+    let { pathname } = url;
+
+    if (pathname.startsWith('/db/')) pathname = pathname.replace('/db/', '');
+
+    let [region, subpage, target, ...paths] = pathname.split('/').filter(Boolean);
+
+    url.hostname = 'apps.atlasacademy.io';
+    let page = await fetch(url.href, request);
+
+    switch (subpage) {
+        case `servant`: {
+            let { name } = await fetch(`https://api.atlasacademy.io/basic/${region}/servant/${target}?lang=en`).then(res => res.json());
+            return overwrite(page, `[${region}] Servant - ${name} - Atlas Academy DB`);
+        }
+        default:
+            return new Response(page.body, page);
     }
 }
