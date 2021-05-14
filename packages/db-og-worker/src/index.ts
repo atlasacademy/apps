6548d83e4e67b7322276d6f98adbd25c37bea7fc
squaresmile <squaresmile@protonmail.com>
2021-05-13 23:15:18 -0400

    Improved og title and description
    - Guess language from request's country
    - Added og:site_name
    - Added tab info to title
    - Added rarity and class name to title

--- packages/db-og-worker/src/index.ts
+++ packages/db-og-worker/src/index.ts
@@ -18,29 +18,52 @@ class Handler {
     }
 }
 
+function toTitleCase(value: string): string {
+    const matches = value.match(/[A-Z]*[a-z0-9]*/g);
+    if (!matches || !matches.length) return value;
+
+    const words = matches
+        .filter((word) => word.length > 0)
+        .map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        });
+
+    return words.join(" ");
+}
+
 async function fetchApi(
     region: string,
     endpoint: string,
     target: string,
-    language: string = "en"
+    language: "jp" | "en" = "en"
 ) {
     const dataType = ["item", "function"].includes(endpoint) ? "nice" : "basic";
     const url = `https://api.atlasacademy.io/${dataType}/${region}/${endpoint}/${target}?lang=${language}`;
     return fetch(url).then((res) => res.json());
 }
 
-function overwrite(response: Response, title: string, image?: string) {
+function overwrite(
+    response: Response,
+    title: string,
+    image?: string,
+    description?: string
+) {
+    const defaultDescription =
+        "Atlas Academy DB - FGO Game Data Navigator - without any of the fluff.";
+    const metaDescription = `${description || title} - ${defaultDescription}`;
+    const ogDescription = description || defaultDescription;
+
     const titleRewriter = new HTMLRewriter()
+        .on('[name="description"]', new Handler(metaDescription))
         .on('[property="og:title"]', new Handler(title))
-        .on('[name="description"]', new Handler(title));
-    if (image !== undefined) {
-        return titleRewriter
-            .on('[property="og:image"]', new Handler(image))
-            .on('[property="og:image:alt"]', new Handler(title))
-            .transform(response);
-    } else {
-        return titleRewriter.transform(response);
-    }
+        .on('[property="og:description"]', new Handler(ogDescription));
+
+    if (image === undefined) return titleRewriter.transform(response);
+
+    return titleRewriter
+        .on('[property="og:image"]', new Handler(image))
+        .on('[property="og:image:alt"]', new Handler(`${title} icon`))
+        .transform(response);
 }
 
 const listingPageTitles = new Map([
@@ -73,7 +96,22 @@ const itemPageTitles = new Map([
     ["quest", { itemType: "Quest", endpoint: "quest" }],
 ]);
 
+const tabTitles = new Map([
+    ["skill-1", "1st Skill"],
+    ["skill-2", "2nd Skill"],
+    ["skill-3", "3rd Skill"],
+    ["passives", "Passive Skills"],
+    ["stat-growth", "Stat Growth"],
+    ["lore", "Profile"],
+    ["assets", "Image Assets"],
+    ["voices", "Voice Lines"],
+]);
+
 async function handleRequest(request: Request) {
+    const language = ["JP", "TW", "CN"].includes(request.cf.country)
+        ? "jp"
+        : "en";
+
     let url = new URL(request.url);
     let { pathname } = url;
 
@@ -94,14 +132,38 @@ async function handleRequest(request: Request) {
 
     const itemPage = itemPageTitles.get(subpage);
     if (itemPage !== undefined) {
-        const { name, longName, face, icon } = await fetchApi(
-            region,
-            itemPage.endpoint,
-            target
-        );
-        const itemName =
-            longName !== undefined ? longName.replace("\n", " ") : name;
-        const title = `[${region}] ${itemPage.itemType} - ${itemName} - Atlas Academy DB`;
+        const res = await fetchApi(region, itemPage.endpoint, target, language);
+        const { name, longName, face, icon, rarity, className } = res;
+
+        let title = `[${region}] ${itemPage.itemType} - ${name}`;
+        switch (subpage) {
+            case "servant":
+                title = `[${region}] ${rarity}★ ${toTitleCase(
+                    className
+                )} - ${name}`;
+                break;
+            case "craft-essence":
+                title = `[${region}] ${rarity}★ Craft Essence - ${name}`;
+                break;
+            case "command-code":
+                title = `[${region}] ${rarity}★ Command Code - ${name}`;
+                break;
+            case "war":
+                const warName = longName.replace("\n", " ");
+                title = `[${region}] ${itemPage.itemType} - ${warName}`;
+                break;
+        }
+
+        if (paths.length > 0) {
+            const tabTitle =
+                tabTitles.get(paths[0]) ||
+                paths[0]
+                    .split("-")
+                    .map((word) => toTitleCase(word))
+                    .join(" ");
+            title = `${title} - ${tabTitle}`;
+        }
+
         return overwrite(page, title, face || icon);
     }
 
@@ -110,23 +172,29 @@ async function handleRequest(request: Request) {
             const { funcId, funcPopupText } = await fetchApi(
                 region,
                 "function",
-                target
+                target,
+                language
             );
             const funcTitle =
                 funcPopupText === ""
                     ? `Function: ${funcId}`
                     : `Function ${funcId}: ${funcPopupText}`;
-            const title = `[${region}] ${funcTitle} - Atlas Academy DB`;
+            const title = `[${region}] ${funcTitle}`;
             return overwrite(page, title);
         }
         case "buff": {
-            const { id, name, icon } = await fetchApi(region, "buff", target);
-            const title = `[${region}] Buff ${id}: ${name} - Atlas Academy DB`;
+            const { id, name, icon } = await fetchApi(
+                region,
+                "buff",
+                target,
+                language
+            );
+            const title = `[${region}] Buff ${id}: ${name}`;
             return overwrite(page, title, icon);
         }
         case "ai": {
             const aiType = target === "svt" ? "Servant" : "Field";
-            const title = `[${region}] ${aiType} AI ${paths[0]} - Atlas Academy DB`;
+            const title = `[${region}] ${aiType} AI ${paths[0]}`;
             return overwrite(page, title);
         }
         default:
