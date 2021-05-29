d6c4280436f19825b7184454b96798bc5bbdcdd1
sadisticsolution <sadisticsolutione@gmail.com>
2021-05-29 14:52:00 -0700

    Fixed import

--- packages/paper-moon/src/paper-moon/BattleManager.ts
+++ packages/paper-moon/src/paper-moon/BattleManager.ts
@@ -2,7 +2,7 @@ import {Servant} from "@atlasacademy/api-connector";
 import BattleServantActor from "@atlasacademy/battle/dist/Actor/BattleServantActor";
 import {Battle} from "@atlasacademy/battle/dist/Battle";
 import {BattleTeam} from "@atlasacademy/battle/dist/Enum/BattleTeam";
-import {BattleActor} from "../../../battle/dist/Actor/BattleActor";
+import {BattleActor} from "@atlasacademy/battle/dist/Actor/BattleActor";
 import {battleSyncThunk} from "../app/battle/thunks";
 import {store} from "../app/store";
 
