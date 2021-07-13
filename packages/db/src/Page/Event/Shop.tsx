e38baff2055b14cb6af45eb53a10042c56d4087c
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-07-13 21:12:01 +0700

    Support filtering by currency type

--- packages/db/src/Page/Event/Shop.tsx
+++ packages/db/src/Page/Event/Shop.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Alert, Button, Form, InputGroup, Table } from "react-bootstrap";
+import { Alert, Button, ButtonGroup, Form, InputGroup, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ItemIcon from "../../Component/ItemIcon";
 import ShopPurchaseDescriptor from "../../Descriptor/ShopPurchaseDescriptor";
@@ -41,6 +41,7 @@ interface IProps {
 
 const ShopTab = ({ region, shops, itemMap, filters, onChange } : IProps) => {
     let [forceEnablePlanner, setForceEnablePlanner] = useState<boolean | undefined>(undefined);
+    let [itemFilters, setItemFilters] = useState(new Set<number>());
 
     let shopEnabled = forceEnablePlanner === undefined ? Manager.shopPlannerEnabled() : forceEnablePlanner;
     let counted = shops
@@ -58,6 +59,10 @@ const ShopTab = ({ region, shops, itemMap, filters, onChange } : IProps) => {
         else
             amounts.set(item.id, amount);
 
+
+    // reset filter if nothing is chosen
+    if (!amounts.size && itemFilters.size) setItemFilters(new Set());
+
     return (
         <>
             <br />
@@ -65,15 +70,37 @@ const ShopTab = ({ region, shops, itemMap, filters, onChange } : IProps) => {
                 {shopEnabled
                     ? (amounts.size ? 'Total amount for chosen items: ' : 'No item was chosen. Choose at least one to get calculations.')
                     : 'Total currency amount needed to clear the shop: '}
-                {[...amounts]
-                    .filter(([_, amount]) => amount)
-                    .map(
-                    ([itemId, amount]) => (
-                        <span style={{ whiteSpace: 'nowrap' }}>
-                            <b>{amount.toLocaleString()}</b> <IconLink region={region} item={items.get(itemId)!} />&nbsp;
-                        </span>
-                    )
-                )}
+                <ButtonGroup>
+                    {[...amounts]
+                        .filter(([_, amount]) => amount)
+                        .map(
+                        ([itemId, amount]) => {
+                            let content = (
+                                <span style={{ whiteSpace: 'nowrap' }}>
+                                    <b>{amount.toLocaleString()}</b> <IconLink region={region} item={items.get(itemId)!} />
+                                </span>
+                            )
+                            if (shopEnabled)
+                                return (
+                                    <Button
+                                        className="shadow-none"
+                                        variant={itemFilters.has(itemId) ? 'outline-dark' : 'light'}
+                                        style={{ backgroundColor: 'transparent', color: 'unset', outline: 'none' }} size="sm"
+                                        onClick={() => {
+                                            let _new = new Set(itemFilters);
+                                            if (_new.has(itemId))
+                                                _new.delete(itemId);
+                                            else
+                                                _new.add(itemId);
+                                            setItemFilters(_new);
+                                        }}>
+                                        {content}
+                                    </Button>
+                                )
+                            return content;
+                        }
+                    )}
+                </ButtonGroup>
                 <div style={{ display: 'inline' }}>
                     <Button variant={shopEnabled ? 'dark' : 'success'} onClick={() => setForceEnablePlanner(!forceEnablePlanner)}>
                         {shopEnabled ? 'Disable planner' : 'Enable planner'}
@@ -94,6 +121,7 @@ const ShopTab = ({ region, shops, itemMap, filters, onChange } : IProps) => {
                 </thead>
                 <tbody>
                     {shops
+                        .filter(shop => (itemFilters.size && amounts.size) ? itemFilters.has(shop.cost.item.id) : true)
                         .sort((a, b) => a.priority - b.priority)
                         .map((shop) => {
                             let limitNumIndicator = shopEnabled
