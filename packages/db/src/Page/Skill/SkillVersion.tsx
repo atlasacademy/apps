5069e5fb144aaf017bf80e398095aa5499cdd76d
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-02-14 21:40:37 +0700

    Make SkillVersion a functional component

--- packages/db/src/Page/Skill/SkillVersion.tsx
+++ packages/db/src/Page/Skill/SkillVersion.tsx
@@ -12,38 +12,36 @@ interface IProps {
     level: number;
 }
 
-class SkillVersion extends React.Component<IProps> {
-    render() {
-        return (
-            <div>
-                {this.props.skill.functions.map((func, index) => {
-                    const dataVal = getTargetVersionValues(func, this.props.level);
+function SkillVersion(props: IProps) {
+    return (
+        <div>
+            {props.skill.functions.map((func, index) => {
+                const dataVal = getTargetVersionValues(func, props.level);
 
-                    return (
-                        <div key={func.funcId}>
-                            <h3>Effect #{index + 1}</h3>
-                            <p>
-                                <FuncDescriptor region={this.props.region} func={func} level={this.props.level}/>
-                            </p>
+                return (
+                    <div key={func.funcId}>
+                        <h3>Effect #{index + 1}</h3>
+                        <p>
+                            <FuncDescriptor region={props.region} func={func} level={props.level}/>
+                        </p>
 
-                            <Row>
-                                <Col xs={12} md={6}>
-                                    <h5>Function</h5>
-                                    <FuncMainData region={this.props.region} func={func}/>
-                                </Col>
-                                <Col xs={12} md={6}>
-                                    <h5>Values</h5>
-                                    <DataValMainData dataVal={dataVal ?? {}}/>
-                                </Col>
-                            </Row>
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <h5>Function</h5>
+                                <FuncMainData region={props.region} func={func}/>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <h5>Values</h5>
+                                <DataValMainData dataVal={dataVal ?? {}}/>
+                            </Col>
+                        </Row>
 
-                            <hr/>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
+                        <hr/>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default SkillVersion;
