9ed00a332e27bfee112e0683733c2ad94b25a2ef
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2022-04-01 22:57:41 +0530

    moved originalName to BgmEntitity in Bgm,ts

--- packages/api-connector/src/Schema/Bgm.ts
+++ packages/api-connector/src/Schema/Bgm.ts
@@ -4,7 +4,6 @@ import { Shop } from "./Shop";
 export interface Bgm {
     id: number;
     name: string;
-    originalName: string;
     fileName: string;
     notReleased: boolean;
     audioAsset?: string;
@@ -23,6 +22,7 @@ export interface BgmRelease {
 export interface BgmEntity {
     id: number;
     name: string;
+    originalName: string;
     fileName: string;
     audioAsset?: string;
     priority: number;
