4db2aab4ca0e67a380bc278a019fc968ff190db7
squaresmile <squaresmile@protonmail.com>
2022-04-13 09:37:22 +0700

    Reverted output file name

--- packages/db-og-worker/webpack.config.js
+++ packages/db-og-worker/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.ts",
     output: {
-        filename: "index.js",
+        filename: "worker.js",
         path: path.join(__dirname, "dist"),
     },
     devtool: "cheap-module-source-map",
