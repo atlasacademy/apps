754d373ba3ce04634a5e1f35dafae218d133a5d4
squaresmile <squaresmile@protonmail.com>
2022-04-13 08:48:12 +0700

    Updated worker to latest template

--- packages/db-og-worker/webpack.config.js
+++ packages/db-og-worker/webpack.config.js
@@ -1,16 +1,15 @@
 const path = require("path");
 
-const mode = process.env.NODE_ENV || "production";
-
 module.exports = {
+    entry: "./src/index.ts",
     output: {
-        filename: `worker.${mode}.js`,
+        filename: "worker.js",
         path: path.join(__dirname, "dist"),
     },
-    mode,
+    devtool: "cheap-module-source-map",
+    mode: "development",
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
-        plugins: [],
     },
     module: {
         rules: [
@@ -18,7 +17,8 @@ module.exports = {
                 test: /\.tsx?$/,
                 loader: "ts-loader",
                 options: {
-                    transpileOnly: true,
+                    // transpileOnly is useful to skip typescript checks occasionally:
+                    // transpileOnly: true,
                 },
             },
         ],
