0ce6cf98a6d1f7a2659681d30e0800d9eee2feeb
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-06-13 21:05:37 +0200

    Added locale zh-tw to parser

--- packages/db/i18next-parser.config.js
+++ packages/db/i18next-parser.config.js
@@ -42,7 +42,7 @@ module.exports = {
     lineEnding: 'auto',
     // Control the line ending. See options at https://github.com/ryanve/eol
   
-    locales: ['en-US', 'zh-CN'],
+    locales: ['en-US', 'zh-CN', 'zh-TW'],
     // An array of the locales in your applications
   
     namespaceSeparator: ':',
