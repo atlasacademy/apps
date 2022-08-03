f9f1a2bababfae2b8175b0fdf103d248dd9b3dcb
boyonthebeach2k <39589879+boyonthebeach2k@users.noreply.github.com>
2022-08-03 14:07:52 +0530

    Fix showFQSpotsOnlyButton

--- packages/db/src/Page/WarMap/WarMap.tsx
+++ packages/db/src/Page/WarMap/WarMap.tsx
@@ -214,8 +214,10 @@ class WarMap extends React.Component<IProps, IState> {
             !doNotGimmicks.includes(this.props.warId) && this.state.isMapLoaded && !!this.state.OGMapGimmicks.length;
 
         const showFQSpotsOnlyButton =
-            this.props.spots.filter((spot) => spot.quests.some((quest) => quest.afterClear === "repeatLast")).length ===
-                this.props.spots.length && this.state.isMapLoaded;
+            this.props.spots.filter((spot) => spot.quests.some((quest) => quest.afterClear === "repeatLast")).length !==
+                this.props.spots.filter(
+                    (spot) => spot.x < this.props.map.mapImageW && spot.y < this.props.map.mapImageH
+                ).length && this.state.isMapLoaded;
 
         const mapImageElement = (
             <>
