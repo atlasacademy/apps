3a8ad056fd0ec162fa5c897ee54d4a51005cdba7
squaresmile <squaresmile@protonmail.com>
2023-01-28 17:45:32 +0700

    Fetch drop-serializer from its build branch

--- netlify_build.sh
+++ netlify_build.sh
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
-CI=false \
-&& mkdir -p src \
-&& cd src \
-&& curl -L -o fgo-lookup.tar.gz https://github.com/jycl1234/fgo-lookup/archive/master.tar.gz \
-&& tar -zxvf fgo-lookup.tar.gz \
-&& cd .. \
-&& mkdir -p app \
-&& cp -r src/fgo-lookup-master/dist app/drop-lookup \
-&& cd src \
-&& curl -L -o drop-serializer.tar.gz https://github.com/atlasacademy/drop-serializer-react/archive/master.tar.gz \
-&& tar -zxvf drop-serializer.tar.gz \
-&& npm install -g npm@9.2.0 \
-&& cd drop-serializer-react-master && npm ci && npm run build \
-&& cd ../../ && cp -r src/drop-serializer-react-master/build/ app/drop-serializer \
-&& cd src \
-&& curl -L -o bingo.tar.gz https://github.com/atlasacademy/bingo/archive/master.tar.gz \
-&& tar -zxvf bingo.tar.gz \
-&& cd ../ && cp -r ./src/bingo-master app/bingo \
-&& cd src \
-&& curl -L -o chargers.tar.gz https://github.com/atlasacademy/chargers/archive/main.tar.gz \
-&& tar -zxvf chargers.tar.gz \
-&& cd chargers-main && npm ci && npm run build \
-&& cd ../../ && cp -r src/chargers-main/out/ app/chargers \
-&& npm ci \
-&& npx lerna bootstrap --ci \
-&& cd packages/db && npm run build \
-&& cd ../../ && cp -r packages/db/build/ app/db \
-&& cd packages/paper-moon && npm run build \
-&& cd ../../ && cp -r packages/paper-moon/build/ app/paper-moon \
-&& cp build/index.html app/index.html \
-&& cp build/_redirects app/_redirects \
-&& cp build/robots.txt app/robots.txt \
-&& cp packages/db/public/favicon.ico app/favicon.ico
+set -e
+
+CI=false
+
+mkdir -p src
+
+mkdir -p app/drop-lookup
+curl -L -o src/fgo-lookup.tar.gz https://github.com/jycl1234/fgo-lookup/archive/master.tar.gz
+tar -C app/drop-lookup --strip-components=2 -zxvf src/fgo-lookup.tar.gz fgo-lookup-master/dist
+
+mkdir -p app/bingo
+curl -L -o src/bingo.tar.gz https://github.com/atlasacademy/bingo/archive/master.tar.gz
+tar -C app/bingo --strip-components=1 -zxvf src/bingo.tar.gz bingo-master
+
+mkdir -p app/drop-serializer
+curl -L -o src/drop-serializer.tar.gz https://github.com/atlasacademy/drop-serializer-react/archive/build.tar.gz
+tar -C app/drop-serializer --strip-components=1 -zxvf src/drop-serializer.tar.gz drop-serializer-react-build
+
+# chargers is hosted on Github Pages
+# cd src
+# curl -L -o chargers.tar.gz https://github.com/atlasacademy/chargers/archive/main.tar.gz
+# tar -zxvf chargers.tar.gz
+# cd chargers-main && npm ci && npm run build
+# cd ../../ && cp -r src/chargers-main/out/ app/chargers
+
+# fgo-docs is hosted on Github Pages
+
+npm ci
+npx lerna bootstrap --ci
+
+cd packages/db && npm run build
+cd ../../ && cp -r packages/db/build/ app/db
+
+cd packages/paper-moon && npm run build
+cd ../../ && cp -r packages/paper-moon/build/ app/paper-moon
+
+cp build/index.html app/index.html
+cp build/_redirects app/_redirects
+cp build/robots.txt app/robots.txt
+cp packages/db/public/favicon.ico app/favicon.ico
