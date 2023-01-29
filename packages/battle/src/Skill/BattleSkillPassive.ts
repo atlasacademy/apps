7b4c7d99a54cee8fde47223b2fafc25bcd5616c5
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-01-29 15:44:52 +0100

    Fix  super() hasn't been called

--- packages/battle/src/Skill/BattleSkillPassive.ts
+++ packages/battle/src/Skill/BattleSkillPassive.ts
@@ -3,25 +3,25 @@ import BattleSkillFunc from "./BattleSkillFunc";
 
 export default class BattleSkillPassive extends BattleSkill {
     constructor(public props: BattleSkillProps, state: BattleSkillState | null) {
-        super(
-            props,
-            state ?? {
-                cooldown: 0,
-                funcs: props.skill.functions.map((func, i) => {
-                    return new BattleSkillFunc(
-                        {
-                            actorId: props.actorId,
-                            func,
-                            level: props.level,
-                            passive: true,
-                        },
-                        null,
-                        this
-                    );
-                }),
-            }
-        );
+        super(props, state);
 
+        const propState = state ?? {
+            cooldown: 0,
+            funcs: props.skill.functions.map((func, _i) => {
+                return new BattleSkillFunc(
+                    {
+                        actorId: props.actorId,
+                        func,
+                        level: props.level,
+                        passive: true,
+                    },
+                    null,
+                    this
+                );
+            }),
+        };
+
+        this.state = propState;
         this.state.funcs.forEach((func) => (func.parent = this));
     }
 
