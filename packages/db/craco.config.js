52c82b13a57e9b358a8ab38e68f7596d8c2d5482
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-02-04 18:58:29 +0100

    Revert "db: update from craco to vite"
    This reverts commit 0b5d1e6200170f13bcdc22b358b138bbfa1224a9.

---
+++ packages/db/craco.config.js
@@ -0,0 +1,29 @@
+const CracoEsbuildPlugin = require('craco-esbuild');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+      options: {
+        includePaths: [], // Optional. If you want to include components which are not in src folder
+        esbuildLoaderOptions: {
+          // Optional. Defaults to auto-detect loader.
+          loader: 'tsx', // Set the value to 'tsx' if you use typescript
+          target: 'es6',
+        },
+        esbuildMinimizerOptions: {
+          // Optional. Defaults to:
+          target: 'es6',
+          css: true, // if true, OptimizeCssAssetsWebpackPlugin will also be replaced by esbuild.
+        },
+        skipEsbuildJest: false, // Optional. Set to true if you want to use babel for jest tests,
+        esbuildJestOptions: {
+          loaders: {
+            '.ts': 'ts',
+            '.tsx': 'tsx',
+          },
+        },
+      },
+    },
+  ],
+};
