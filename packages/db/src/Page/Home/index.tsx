49d865f343ae29485b94b75d2ce0f243a0883cfb
squaresmile <squaresmile@protonmail.com>
2023-02-04 10:37:00 +0700

    Fixed eqeqeq

--- packages/db/src/Page/Home/index.tsx
+++ packages/db/src/Page/Home/index.tsx
@@ -4,4 +4,4 @@ import homeEN from "./en";
 import homeZHCN from "./zhCN";
 
 export const home = (uiLang: UILanguage) =>
-    uiLang === UILanguage.ZH_CN || uiLang == UILanguage.ZH_TW ? homeZHCN : homeEN;
+    uiLang === UILanguage.ZH_CN || uiLang === UILanguage.ZH_TW ? homeZHCN : homeEN;
