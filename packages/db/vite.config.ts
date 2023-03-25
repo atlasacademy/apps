26aac00f82bdbdbd734b5ccf3d053b034be243ea
squaresmile <squaresmile@protonmail.com>
2023-03-25 18:56:12 +0700

    Switched from CRA to vite
    commit 6f657343ad9a0dd365848b36a3194a75c0fdacd4
    Author: squaresmile <squaresmile@protonmail.com>
    Date:   Sat Mar 25 14:23:26 2023 +0700
    
        Switched to vite plugin-react-swc
    
    commit 15cd6e6323428750595ef6ae75c50e8b6e26a6e8
    Author: squaresmile <squaresmile@protonmail.com>
    Date:   Sat Mar 25 14:00:25 2023 +0700
    
        Lowered target to ios11
    
    commit 1741d22c6e2dc1d0af7986395456160229553bc2
    Author: squaresmile <squaresmile@protonmail.com>
    Date:   Sat Mar 25 13:40:26 2023 +0700
    
        Added eslint to vite
    
    commit 6a267cef8a1638bb8c53370d04d43e031183d6b6
    Author: squaresmile <squaresmile@protonmail.com>
    Date:   Sat Mar 25 13:27:26 2023 +0700
    
        Removed serviceworker.ts
    
    commit bc5eef6489e89df69b388383f0ea39eadeaa03d2
    Author: squaresmile <squaresmile@protonmail.com>
    Date:   Sat Mar 25 13:18:45 2023 +0700
    
        Switched from cra to vite

---
+++ packages/db/vite.config.ts
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import eslint from "vite-plugin-eslint";
+import svgrPlugin from "vite-plugin-svgr";
+
+export default defineConfig({
+    base: "/db/",
+    plugins: [react(), eslint(), svgrPlugin(), splitVendorChunkPlugin()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: "ios11",
+        outDir: "build",
+        commonjsOptions: {
+            include: [],
+        },
+    },
+    optimizeDeps: {
+        // https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
+        include: ["@atlasacademy/api-connector", "@atlasacademy/api-descriptor"],
+        // https://vitejs.dev/config/dep-optimization-options.html#optimizedeps-disabled
+        // Setting this to false should enable optimizeDeps for both dev and build.
+        // This way esbuild will convert dependencies to esm and we don't have to deal with @rollup/plugin-commonjs
+        disabled: false,
+    },
+});
