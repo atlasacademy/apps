d47dd1cae7de4d8c2c4b60dca974c691da411c58
boyonthebeach2k <39589879+boyonthebeach2k@users.noreply.github.com>
2023-07-31 17:55:06 +0530

    Update location of drop lookup tool

--- netlify_build.sh
+++ netlify_build.sh
@@ -7,7 +7,7 @@ CI=false
 mkdir -p src
 
 mkdir -p app/drop-lookup
-curl -L -o src/fgo-lookup.tar.gz https://github.com/jycl1234/fgo-lookup/archive/master.tar.gz
+curl -L -o src/fgo-lookup.tar.gz https://github.com/atlasacademy/fgo-lookup/archive/master.tar.gz
 tar -C app/drop-lookup --strip-components=2 -zxvf src/fgo-lookup.tar.gz fgo-lookup-master/dist
 
 mkdir -p app/bingo
