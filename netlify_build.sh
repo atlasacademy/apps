e9dcbde337ce72fbcc60823ce8b07d7a8ec3dac1
squaresmile <squaresmile@protonmail.com>
2023-08-03 07:45:57 +0700

    Revert "Build lookup page"
    This reverts commit ba2d80f733b13673055b0947f89bd53f275fa414.

--- netlify_build.sh
+++ netlify_build.sh
@@ -9,7 +9,6 @@ mkdir -p src
 mkdir -p app/drop-lookup
 curl -L -o src/fgo-lookup.tar.gz https://github.com/atlasacademy/fgo-lookup/archive/master.tar.gz
 tar -C app/drop-lookup --strip-components=2 -zxvf src/fgo-lookup.tar.gz fgo-lookup-master/dist
-npm run build
 
 mkdir -p app/bingo
 curl -L -o src/bingo.tar.gz https://github.com/atlasacademy/bingo/archive/master.tar.gz
