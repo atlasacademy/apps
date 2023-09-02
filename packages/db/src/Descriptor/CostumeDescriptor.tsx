568bf3622e08d9c0c3c781a8e902f5be2fd32c2b
squaresmile <squaresmile@protonmail.com>
2023-09-02 11:30:16 -0400

    Use short name for costume descriptions
    Since that name is translated

--- packages/db/src/Descriptor/CostumeDescriptor.tsx
+++ packages/db/src/Descriptor/CostumeDescriptor.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from "react-i18next";
+
 import { Region } from "@atlasacademy/api-connector";
 
 import useApi from "../Hooks/useApi";
@@ -12,23 +14,23 @@ export default function CostumeDescriptor(props: {
     tab?: string;
 }) {
     const { data: servant } = useApi("servant", props.servantId);
-    if (servant !== undefined) {
-        let costume = undefined;
-        if (servant.profile !== undefined) {
-            costume = Object.values(servant.profile.costume).find((costume) => costume.id === props.costumeLimit);
-        }
-        return (
-            <>
-                <ServantDescriptor
-                    region={props.region}
-                    servant={servant}
-                    iconHeight={props.iconHeight}
-                    tab={props.tab ?? "lore"}
-                />{" "}
-                costume: {costume ? <span lang={lang(props.region)}>{costume.name}</span> : props.costumeLimit}
-            </>
-        );
-    } else {
-        return null;
+    const { t } = useTranslation();
+
+    if (servant === undefined) return null;
+
+    let costume = undefined;
+    if (servant.profile !== undefined) {
+        costume = Object.values(servant.profile.costume).find((costume) => costume.id === props.costumeLimit);
     }
+    return (
+        <>
+            <ServantDescriptor
+                region={props.region}
+                servant={servant}
+                iconHeight={props.iconHeight}
+                tab={props.tab ?? "lore"}
+            />{" "}
+            {t("costume")}: <span lang={lang(props.region)}>{costume?.shortName ?? props.costumeLimit}</span>
+        </>
+    );
 }
